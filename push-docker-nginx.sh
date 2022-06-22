2b4362b0c32f046f6516bda96adbc06387926b73
gui-sousa <guira_naruto@hotmail.com>
2022-06-22 11:01:58 -0300

    Ajustado script

--- push-docker-nginx.sh
+++ push-docker-nginx.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
 
-# Execute os seguinte comandos para permitir o terminal local
+#Execute os seguinte comandos para permitir o terminal local
 
-# gcloud auth configure-docker \
-#    southamerica-east1-docker.pkg.dev -Y 
+ gcloud auth configure-docker \
+    southamerica-east1-docker.pkg.dev 
 
+#Coloca TAG na imagem docker a ser enviada
 docker tag guisousa/nginx-hello-world:latest southamerica-east1-docker.pkg.dev/develop-348618/nginx-reppo/guisousa/nginx-hello-world:latest
 
+#Faz o envio da imagem para o repositorio
 docker push southamerica-east1-docker.pkg.dev/develop-348618/nginx-reppo/guisousa/nginx-hello-world:latest
 
 
