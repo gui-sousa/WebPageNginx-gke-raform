d2ac8451bb5e7c86674bc8bfa133bb4bdeb9d084
gui-sousa <guira_naruto@hotmail.com>
2022-06-23 17:16:37 -0300

    Criado deployment em HCL

--- main.tf
+++ main.tf
@@ -12,9 +12,19 @@ provider "google-beta" {
 }
 
 data "google_client_config" "provider" {
-  
+
 }
 
 provider "kubernetes" {
-  
+  host  = google_container_cluster.cluster-nginx.endpoint
+  token = data.google_client_config.provider.access_token
+
+  client_certificate = base64decode(
+    google_container_cluster.cluster-nginx.master_auth[0].client_certificate,
+  )
+
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.cluster-nginx.master_auth[0].cluster_ca_certificate,
+  )
+  client_key = base64decode(google_container_cluster.cluster-nginx.master_auth[0].client_key)
 }
